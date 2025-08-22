15965d6da189d05dc236a488f6735050fcd978ea
rasanchez23 <142275585+rasanchez23@users.noreply.github.com>
2025-08-22 08:17:35 -0700

    Update practica.dart

--- practica.dart
+++ practica.dart
@@ -1,1 +1,28 @@
+void main() {
+  double resultadosSumas = sumar(5, 10);
+  print('El resultado de la suma es: $resultadosSumas');
+  
+  double resultadosRestas = restar(20, 8);
+  print('El resultado de la resta es: $resultadosRestas');
+  
+  double resultadosMultiplicacion = multiplicar(8, 9);
+  print('El resultado de la multiplicación es: $resultadosMultiplicacion');
+  
+  double resultadosDivision = dividir(30, 6);
+  print('El resultado de la división es: $resultadosDivision');
+}
 
+double sumar(double a, double b) {
+  return a + b;
+}
+double restar(double a, double b){
+  return a - b;
+}
+  
+double multiplicar(double a, double b){
+  return a * b;
+}
+  
+double dividir(double a, double b){
+  return a / b;
+}
